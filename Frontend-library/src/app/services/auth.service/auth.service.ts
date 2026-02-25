037acc14ca06c64139d4cb22fbb93759c0bf79fa
Leonardo <leonardorodriguez172021@gmail.com>
2026-02-25 10:55:16 -0400

    solucionado error con el token al recargar la pagina

--- Frontend-library/src/app/services/auth.service/auth.service.ts
+++ Frontend-library/src/app/services/auth.service/auth.service.ts
@@ -5,18 +5,13 @@ import { isPlatformBrowser } from '@angular/common';
   providedIn: 'root'
 })
 export class AuthService {
-  // Inyectamos el ID de la plataforma
   private platformId = inject(PLATFORM_ID);
 
   isLoggedIn(): boolean {
-    // Verificamos si estamos en el navegador
     if (isPlatformBrowser(this.platformId)) {
       const token = localStorage.getItem('token');
-      return !!token; // Retorna true si existe, false si no
+      return !!token; 
     }
-    
-    // Si estamos en el servidor, por defecto decimos que no está logueado
-    // (o manejamos la lógica de cookies si fuera necesario)
     return false;
   }
 }
